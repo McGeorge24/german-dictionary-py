a9933771a45b149406fa79209c46163cb0d50c82
McGeorge24 <steiner.jure@gmail.com>
2024-05-27 21:51:08 +0200

    Začel z uporabo tabel

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from openpyxl import load_workbook
+
+
 def main() -> None:
+    # Naloži zvezek in list
+    workbook = load_workbook('assets\\test_spreadsheet.xlsx')
+    letnik = workbook["LETNIK1"]
+
+    # Manipuliranje s celicami
+    letnik["A1"] = "Hello excel"
     print("hello world")
+
+    # Shrani zvezek
+    workbook.save('assets\\test_spreadsheet.xlsx')
 
 
 if __name__ == "__main__":
