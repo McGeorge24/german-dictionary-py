7a667f01a9e3ffce204cab4d07906c7fd945cd6b
McGeorge24 <steiner.jure@gmail.com>
2024-05-28 22:01:47 +0200

    Začel orodja za urejanje celic

---
+++ UrediCelice.py
@@ -0,0 +1,42 @@
+import openpyxl as excel
+import openpyxl.styles as styles
+
+
+class Style:
+    def __init__(self, tip: str):
+        self.tip = tip
+        # za naslov
+        if tip.lower() == "naslov":
+            self.font = styles.Font(
+                name='Arial', size=20, bold=True, color='000000')
+            self.fill = styles.PatternFill(
+                start_color='AA88FF', end_color='AA88FF', fill_type='solid')
+            thick = styles.Side("thick")
+            self.border = styles.Border(
+                left=thick, right=thick, top=thick, bottom=thick)
+            self.alignment = styles.Alignment(
+                horizontal='center', vertical='center')
+        # za vse ostalo
+        elif tip.lower() == "podnaslov":
+            self.font = styles.Font(name='Arial', size=12, color='000000')
+            self.fill = styles.PatternFill()
+            thin = styles.Side("thin")
+            self.border = styles.Border(left=thin, right=thin, bottom=thin)
+            self.alignment = styles.Alignment(
+                horizontal="center", vertical="center")
+        else:
+            self.font = styles.Font(name='Arial', size=12, color='000000')
+            self.fill = None
+            self.border = None
+            self.alignment = styles.Alignment(
+                horizontal='left', vertical='center')
+
+
+def ApplyStyleToCells(sheet, start_row, start_col, end_row, end_col, style: Style) -> None:
+    for row in range(start_row, end_row + 1):
+        for col in range(start_col, end_col + 1):
+            cell = sheet.cell(row=row, column=col)
+            cell.font = style.font
+            cell.fill = style.fill
+            cell.border = style.border
+            cell.alignment = style.alignment
--- UstvariLetnik.py
+++ UstvariLetnik.py
@@ -1,39 +1,16 @@
 import openpyxl as excel
 import openpyxl.styles as styles
+from UrediCelice import *
 
 
-class Style:
-    def __init__(self, tip: str):
-        self.tip = tip
-        # za naslov
-        if tip.lower() == "naslov":
-            self.font = styles.Font(
-                name='Arial', size=20, bold=True, color='000000')
-            self.fill = styles.PatternFill(
-                start_color='AA88FF', end_color='AA88FF', fill_type='solid')
-            self.border = styles.Border(
-                left=styles.Side("thick"), right=styles.Side("thick"), top=styles.Side("thick"), bottom=styles.Side("thick"))
-            self.alignment = styles.Alignment(
-                horizontal='center', vertical='center')
-        # za vse ostalo
-        else:
-            self.font = styles.Font(name='Arial', size=12, color='000000')
-            self.fill = None
-            self.border = None
-            self.alignment = styles.Alignment(
-                horizontal='left', vertical='center')
-
-
-def NaslovPridevnik(tabela, cursor_pos: str, style: Style) -> None:
+def NaslovPridevnik(tabela, cursor_pos: str, naslov: Style, podnaslov: Style) -> None:
 
     tabela["B3"] = "PRIDEVNIKI"
     tabela.merge_cells('B3:C3')
-    print(style.tip)
-    tabela["B3"].font = style.font
-    tabela["B3"].fill = style.fill
-    tabela["B3"].border = style.border
-    tabela["C3"].border = style.border
-    tabela["B3"].alignment = style.alignment
+    ApplyStyleToCells(tabela, 3, 2, 3, 3, naslov)
+    tabela["B4"] = "pridevnik"
+    tabela["C4"] = "pomen"
+    ApplyStyleToCells(tabela, 4, 2, 4, 3, podnaslov)
 
 
 def NaslovGlagol() -> None:
@@ -53,11 +30,12 @@ def NovLetnik(workbook, trenuten_letnik: int) -> None:
     nov_letnik = workbook[f"{trenuten_letnik+1}.letnik"]
 
     slog_naslov = Style("naslov")
+    slog_podnaslov = Style("podnaslov")
     nov_letnik["A1"] = "Letnik:"
     nov_letnik["B1"] = trenuten_letnik+1
     cursor_pos = "B3"
 
-    NaslovPridevnik(nov_letnik, cursor_pos, slog_naslov)
+    NaslovPridevnik(nov_letnik, cursor_pos, slog_naslov, slog_podnaslov)
     NaslovGlagol()
     NaslovPrislov()
     NaslovDrugo()
